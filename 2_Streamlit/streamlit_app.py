2db41ac05cf440ecb78f393241cf78b59d7df0e0
unknown <benedictusaryo@outlook.com>
2019-11-04 11:06:02 +0700

    finished streamlit

--- 2_Streamlit/streamlit_app.py
+++ 2_Streamlit/streamlit_app.py
@@ -9,3 +9,24 @@ This app, predicted Iris Flower based on Sepal Width, Sepal Length, Petal Width,
 '''
 st.title(app_title)
 st.write(app_desc)
+
+# Get values using browser arguments
+sepLen = st.number_input('sepal_length', min_value=0.01, step=0.01)
+sepWid = st.number_input('sepal_width', min_value=0.01, step=0.01)
+petLen = st.number_input('petal_length', min_value=0.01, step=0.01)
+petWid = st.number_input('petal_width', min_value=0.01, step=0.01)
+
+# Concate the Input Data
+# input_data =
+# st.write(sepLen, sepWid, petLen, petWid)
+
+
+output = model.predict([[sepLen, sepWid, petLen, petWid]])
+st.header('Result')
+
+
+st.write(output)
+
+
+# How to use
+# streamlit run streamlit_app.py
