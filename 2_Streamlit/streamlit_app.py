0a147dbcbacce3a42ecd3b2a90f169e2fe0b5849
unknown <benedictusaryo@outlook.com>
2019-11-05 13:19:50 +0700

    use slider

--- 2_Streamlit/streamlit_app.py
+++ 2_Streamlit/streamlit_app.py
@@ -11,22 +11,23 @@ st.title(app_title)
 st.write(app_desc)
 
 # Get values using browser arguments
-sepLen = st.number_input('sepal_length', min_value=0.01, step=0.01)
-sepWid = st.number_input('sepal_width', min_value=0.01, step=0.01)
-petLen = st.number_input('petal_length', min_value=0.01, step=0.01)
-petWid = st.number_input('petal_width', min_value=0.01, step=0.01)
+sepLen = st.slider('sepal_length', min_value=4.30, max_value=7.90)
+sepWid = st.slider('sepal_width', min_value=2.00, max_value=4.40)
+petLen = st.slider('petal_length', min_value=1.00, max_value=6.90)
+petWid = st.slider('petal_width', min_value=0.10, max_value=2.50)
 
 # Concate the Input Data
 # input_data =
 # st.write(sepLen, sepWid, petLen, petWid)
 
-
-output = model.predict([[sepLen, sepWid, petLen, petWid]])
 st.header('Result')
 
+output = model.predict([[sepLen, sepWid, petLen, petWid]])
 
-st.write(output)
-
+if st.button('Predict the Result'):
+    st.write(output)
+else:
+    st.write('Press the button to predict the result')
 
 # How to use
 # streamlit run streamlit_app.py
