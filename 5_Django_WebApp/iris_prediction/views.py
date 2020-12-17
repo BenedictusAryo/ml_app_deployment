772edcd3d918f77607a7250e834abda5f8ec6b3e
BenedictusAryo <benedictusaryo@outlook.com>
2020-12-17 23:01:34 +0700

    :bug: Deploy Django using new model

--- 5_Django_WebApp/iris_prediction/views.py
+++ 5_Django_WebApp/iris_prediction/views.py
@@ -4,6 +4,23 @@ from django.utils import timezone
 import pandas as pd 
 from .models import PredResults
 from django.contrib.auth.decorators import login_required
+import os
+import pickle
+
+################  Model File and Folder Location  ###################
+MODEL_FILE = 'iris_model.pkl'
+MODEL_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, os.pardir, MODEL_FILE))
+
+
+####################  Model Class  #############################
+class IrisModel:
+    def __init__(self, model_path):
+        self.model = pickle.load(open(model_path, 'rb'))
+
+    def predict(self, features):
+        return self.model.predict(features)[0]
+
+model = IrisModel(MODEL_PATH)  
 
 # Create your views here.
 
@@ -20,13 +37,10 @@ def predict_chances(request):
         petal_length = float(request.POST.get('petal_length'))
         petal_width = float(request.POST.get('petal_width'))
 
-        # Unpickle model
-        model = pd.read_pickle(r'C:\Users\Benedict\github\ml_app_deployment\5_Django_WebApp\iris_decTree.pkl')
-
         # Make Prediction
         result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
 
-        classification = result[0].capitalize()
+        classification = result.capitalize()
 
         PredResults.objects.create(pred_date=timezone.now(),sepal_length=sepal_length, sepal_width=sepal_width,
                                    petal_length=petal_length, petal_width=petal_width,
