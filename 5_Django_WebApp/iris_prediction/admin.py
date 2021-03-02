b6f62d7886ee200b1a49e3fa1147058bcee4b779
BenedictusAryo <benedictusaryo@outlook.com>
2021-03-02 19:46:11 +0700

    add models to admin view

--- 5_Django_WebApp/iris_prediction/admin.py
+++ 5_Django_WebApp/iris_prediction/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+for model in apps.get_app_config('iris_prediction').get_models():
+    admin.site.register(model)
